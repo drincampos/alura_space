18dbe21e592e4c0eb73273b13c346e7b9d284a07
drincampos <adriano.campos@gmail.com>
2025-08-29 12:53:15 -0300

    Criado o banco de dados

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from galeria.models import Fotografia
 
-# Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    dados = {
+        1: {"nome": "Nebulosa de Carina", "legenda": "webbtelecope.org / NASA / James Webb"},
+        2: {"nome": "Galáxia NGC 1079", "legenda": "nasa.org / NASA / Hubble"}
+    }
+    fotografias = Fotografia.objects.all()
+    return render(request, 'index.html', {"cards": fotografias})
 
 def imagem(request):
     return render(request, 'imagem.html')
