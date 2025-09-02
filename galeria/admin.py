cab89e5edf672a42938045656d9755c1fe29d310
drincampos <adriano.campos@gmail.com>
2025-09-02 09:41:39 -0300

    Implementado o filtro de publicação

--- galeria/admin.py
+++ galeria/admin.py
@@ -3,9 +3,10 @@ from .models import Fotografia
 
 
 class ListaFotografias(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'legenda')
+    list_display = ('id', 'nome', 'legenda', 'publicada')
     list_display_links = ('id', 'nome')
     search_fields = ('nome', )
     list_filter = ('categoria',)
+    list_editable = ('publicada',)
 
 admin.site.register(Fotografia, ListaFotografias)
--- galeria/views.py
+++ galeria/views.py
@@ -2,11 +2,7 @@ from django.shortcuts import render
 from galeria.models import Fotografia
 
 def index(request):
-    dados = {
-        1: {"nome": "Nebulosa de Carina", "legenda": "webbtelecope.org / NASA / James Webb"},
-        2: {"nome": "Galáxia NGC 1079", "legenda": "nasa.org / NASA / Hubble"}
-    }
-    fotografias = Fotografia.objects.all()
+    fotografias = Fotografia.objects.filter(publicada=True)
     return render(request, 'index.html', {"cards": fotografias})
 
 def imagem(request):
