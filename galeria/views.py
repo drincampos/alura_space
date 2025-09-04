b58da4d180d589d754d34b37d207c6f2f8f7de5e
drincampos <adriano.campos@gmail.com>
2025-09-04 11:48:10 -0300

    Versão final com busca

--- galeria/views.py
+++ galeria/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia
 
 def index(request):
     fotografias = Fotografia.objects.filter(publicada=True)
     return render(request, 'index.html', {"cards": fotografias})
 
-def imagem(request):
-    return render(request, 'imagem.html')
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'imagem.html', {"fotografia": fotografia})
+
+def buscar(request):
+    fotografias = Fotografia.objects.order_by('data_foto').filter(publicada=True)
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar:
+            fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+    return render(request, 'buscar.html', {"cards": fotografias})
